e8ac8c6a41ef60759afcc3672af7e4b660e17b60
ramason06 <ahramson94@gmail.com>
2024-07-08 21:00:51 +0900

    maxDepthOfBT.go, sameTree.go

---
+++ easy/maxDepthOfBT.go
@@ -0,0 +1,15 @@
+package easy
+
+import "leetcodeGo/common"
+
+func maxDepth(root *common.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDepth(root.Left)
+	r := maxDepth(root.Right)
+	if l > r {
+		return l + 1
+	}
+	return r + 1
+}
---
+++ easy/sameTree.go
@@ -0,0 +1,16 @@
+package easy
+
+import "leetcodeGo/common"
+
+func isSameTree(p *common.TreeNode, q *common.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
